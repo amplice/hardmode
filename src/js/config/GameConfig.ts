87c78db9e7cfe5420884a5a2eb5f16362ba77cb5
amplice <amplice@protonmail.com>
2025-07-21 00:37:08 +0100

    Adjust DarkMage shadowbolt sprite size and orientation
    - Reduce shadowbolt sprite scale from 1.2 to 0.9
    - Flip sprite horizontally (flipX: true)

--- src/js/config/GameConfig.ts
+++ src/js/config/GameConfig.ts
@@ -724,12 +724,12 @@ export const PLAYER_CONFIG = {
       flipY: false
     },
     darkmage_shadowbolt_effect: {
-      scale: 1.2,
+      scale: 0.9,
       offsetDistance: 35,
       rotationOffset: 0,
       animationSpeed: 0.4,
       followDuration: 0,
-      flipX: false,
+      flipX: true,
       flipY: false
     },
     elemental_spell_effect: {
