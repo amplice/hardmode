437b3c268b5b8c494da77082479f70dfc6ca9f3e
amplice <amplice@protonmail.com>
2025-09-05 20:32:14 +0100

    Fix swapped Ogre and Winged Demon attack sounds
    - Ogre basic attack now uses spin sound, special uses melee sound
    - Winged Demon basic attack now uses special sound, spell uses melee sound

--- src/js/config/SoundConfig.ts
+++ src/js/config/SoundConfig.ts
@@ -92,13 +92,13 @@ export const SOUND_CONFIG = {
         pool: 5
     },
     'ogre_attack': {
-        src: 'assets/sounds/monsters/ogre_melee_attack.wav',
+        src: 'assets/sounds/monsters/ogre_spin_attack.ogg',  // Swapped - basic attack uses spin sound
         category: SoundCategory.SFX,
         priority: SoundPriority.MEDIUM,
         pool: 5
     },
     'ogre_spin': {
-        src: 'assets/sounds/monsters/ogre_spin_attack.ogg',
+        src: 'assets/sounds/monsters/ogre_melee_attack.wav',  // Swapped - special uses melee sound
         category: SoundCategory.SFX,
         priority: SoundPriority.MEDIUM,
         pool: 3
@@ -140,13 +140,13 @@ export const SOUND_CONFIG = {
         pool: 3
     },
     'wingeddemon_attack': {
-        src: 'assets/sounds/monsters/winged_demon_melee_attack.wav',
+        src: 'assets/sounds/monsters/winged_demon_special_attack.wav',  // Swapped - basic attack uses special sound
         category: SoundCategory.SFX,
         priority: SoundPriority.MEDIUM,
         pool: 5
     },
     'wingeddemon_spell': {
-        src: 'assets/sounds/monsters/winged_demon_special_attack.wav',
+        src: 'assets/sounds/monsters/winged_demon_melee_attack.wav',  // Swapped - special uses melee sound
         category: SoundCategory.SFX,
         priority: SoundPriority.MEDIUM,
         pool: 5
